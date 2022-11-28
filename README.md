6e6bce3f193f90af551d9a4a4486c0f019419e45
Levi Ob <commonnightingale@pm.me>
2022-11-28 14:46:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Introduction
+In this project, I deliver a program which automates the analysis of true barcodes. This program accepts a long list of apparent barcodes in FASTQ format and produces a list of true barcodes. 
+
+# Background
+[MAPseq](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6640135/) is a technique for mapping of neuron connections in the brain. This technique is advantageous to conventional neuroanatomical tracing methods in several ways, including its high throughput. In MAPseq, random RNA nucleotide sequences known as "barcodes" are generated within the main neuron bodies (somas) using a virus. The barcodes then spread to the neuron's axons. By matching the locations of these unique barcodes (both in the soma, and other regions), the long-range connections of many individual neurons can be mapped. If a barcode exists in both the soma within one region, and also occurs in another region of the brain, then an axon is very likely connecting the soma to that region. Due to the uniqueness of randomly created RNA sequences, it is unlikely for two barcode sequences to be identical. As the number of cells being mapped increases, the likelihood of similar barcodes increases. The generation of barcodes is a biologic process, completed after injection, so the exact barcodes are not known in advance. 
+
+Potential barcodes are able to be identified by anchor sequences. A characteristic of the virus which is producing barcodes is a known string of nucleotide base pairs. These known anchor sequences occur adjacent to barcodes. In other words, a potential barcode is defined as the base pairs immediately preceding the anchor sequence. The datasets used are from experiments with 30 nucleotide barcodes and a 19 nucleotide anchor sequence (GTACTGCGGCCGCTACCTA). By searching for this anchor sequence in the sequencing data, we can deduce that the preceding 30 base pairs are a potential barcode. The provided data has already been parsed for any pattern loosely matching the anchor sequence. Each 4 lines is one read, including the potential barcode and anchor sequence. 
+
+Errors are imminent during the sequencing and transcription processes. Because of this, error correction may be applied to combine barcodes which have only a few incorrect base pairs. 
+
+
+# File descriptions
+The notebook `barcodes.ipynb` is used to show the thought process behind building the solution, and where the justifications are laid out before they are implemented. It also includes considerations and examples of optimizations, and observations of the dataset. The result is `find_barcodes.py`, a script that can be used through the command line.
+
+# Solution
+The true barcodes are expected to have a higher count relative to errors in the data. The most frequently occurring barcodes are most likely the true barcodes. 
+
+By default, error correction is applied by grouping less common barcodes into more common barcodes within 1 Hamming distance of each other. The benefit of correcting similar barcodes is situational, so error correction can be increased or disabled. Disabling error correction will simply return the most common explicit barcodes.
+
+It is not necessary to compare all barcodes with all other barcodes in order to get an accurate picture of the true barcodes. Due to the heavily skewed distribution of the dataset, we can consider only a portion of the most common barcodes and get nearly the same results, but with much better runtime. Comparing all barcodes might achieve more accurate results, but there will still exist uncertainty, especially near the threshold for true barcodes. 
+
+All of these thoughts are expanded upon in `barcodes.ipynb`.
+
+# How to run
+This program was developed using Python 3.8 without any external requirements. It can be run on any machine with Python installed; version 3.8 is recommended. It can be invoked by running 
+
+```
+python3 find_barcodes.py input anchor cell_count
+``` 
+
+from the command line. 
+
+`input` is the filepath of a fastq file to find barcodes in. 
+
+`anchor` is the anchor sequence. 
+
+`cell_count` is the number of cells sequenced (the expected number of true barcodes).
+
+
+Additional arguments can also be passed:
+
+```
+usage: find_barcodes.py [-h] [-e ERROR_HAMMING_DISTANCE] [-p PROPORTION_TO_CONSIDER] [-o OUTPUT] input anchor cell_count
+
+positional arguments:
+  input                 filepath to FASTQ file
+  anchor                the known anchor sequence
+  cell_count            number of cells sequenced/expected true barcodes
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -e ERROR_HAMMING_DISTANCE, --error-hamming-distance ERROR_HAMMING_DISTANCE
+                        Hamming distance used for error correction, default is 1, 0 disables error correction
+  -p PROPORTION_TO_CONSIDER, --proportion-to-consider PROPORTION_TO_CONSIDER
+                        number of barcode groups to create during error correction proportional to cell-count, default is 1 (error-correction stops once cell_count number of groups are created), this argument is ignored when error_hamming_distance is zero
+  -o OUTPUT, --output OUTPUT
+                        directory to save output list of true barcodes, default is with input file
+```
+
+`error_hamming_distance` is the Hamming distance between similar barcodes which will be grouped together. The error distance is 1 by default. Error correction can be disabled by setting this to 0.
+
+`proportion_to_consider` is essentially how thorough the error correction is. The default value of 1 will do the minimum number of comparissons necessary in order to return `cell_count` number of barcode groups. Higher values will consider more barcodes, resulting in results that align more closely with the entire dataset, at the expense of runtime. For example, when `proportion_to_consider` is 3 and `cell_count` is 500, the program will continue grouping barcodes until it creates 1500 barcode groups. The top 500 barcode groups are then output. With a high enough value, the program will compare every barcode to every other barcode. This is not recommended, as runtime increases quadratically proportional to number of barcodes compared. 
+
+`output` is the directory to save the results in and is by default the same directory that the input file is in. Linux, Mac, and Windows path syntax are accepted. The output file is always named `output.txt` and will overwrite existing files in the output directory with the same name. 
+
+Example on Linux:
+```
+$ python3 find_barcodes.py ./data_1M.fastq GTACTGCGGCCGCTACCTA 350 -o ./output_dir -p 3
+350 true barcodes found. Results located in /home/user/find-barcodes/output_dir/output.txt
+```
---
+++ find_barcodes.py
@@ -0,0 +1,206 @@
+"""
+Find and save the true barcodes from a list of apparent barcodes in FASTQ format
+"""
+
+import argparse
+from collections import Counter
+from pathlib import Path
+from typing import Optional
+
+
+def find_barcodes(
+    input_file: Path,
+    anchor: str,
+    cell_count: int,
+    error_hamming_distance: int,
+    proportion_to_consider: float,
+    output_directory: Optional[Path],
+):
+    """
+    Takes file, anchor sequence, cell count, allowable Hamming distance for error correction,
+    proportion of barcodes to consider for error correction, and optional output directory,
+    saves text file of true barcodes and prints true barcode count and directory to file.
+    """
+
+    explicit_barcodes_counter = extract_barcodes(input_file, anchor)
+
+    if error_hamming_distance > 0:
+        explicit_barcodes_counter = group_by_hamming_distance(
+            explicit_barcodes_counter,
+            cell_count,
+            error_hamming_distance,
+            proportion_to_consider,
+        )
+
+    true_barcodes = get_true_barcodes(explicit_barcodes_counter, cell_count)
+
+    write_to_file(true_barcodes, input_file, output_directory)
+
+
+def extract_barcodes(input_file, anchor):
+    """
+    Takes filepath to a list of apparent barcodes and anchor, returns a counter of explicit
+    barcode counts.
+    """
+
+    explicit_barcodes_counter = Counter()
+    with open(input_file) as file:
+        for line_number, line in enumerate(file):
+            # check for anchor on every fourth line
+            if (line_number + 3) % 4 == 0:
+                anchor_index = line.find(anchor, 30)
+                if anchor_index != -1:
+                    barcode = line[anchor_index - 30 : anchor_index]
+                    explicit_barcodes_counter[barcode] += 1
+    return explicit_barcodes_counter
+
+
+def group_by_hamming_distance(
+    explicit_barcodes_counter, cell_count, error_hamming_distance, proportion_to_consider
+):
+    """
+    Takes barcodes counter, cell count, allowable Hamming distance, proportion of barcodes to
+    consider for error correction, and returns a counter of apparent barcodes grouped within
+    the allowable Hamming distance, and their counts.
+    """
+
+    max_number_of_barcode_groups = cell_count * proportion_to_consider
+    grouped_barcodes = {}
+    barcode_groups = 0
+    for explicit_barcode, explicit_count in sorted(
+        explicit_barcodes_counter.items(), key=lambda x: x[1], reverse=True
+    ):
+        # Stop grouping and return once we reach this number of total barcode groups
+        if barcode_groups > max_number_of_barcode_groups:
+            return grouped_barcodes
+        if not grouped_barcodes:
+            grouped_barcodes[explicit_barcode] = explicit_count
+            continue
+        neighbor = find_hamming_neighbor(
+            explicit_barcode, grouped_barcodes, error_hamming_distance
+        )
+        # If there exists a neighbor barcode in grouped_barcodes
+        if neighbor is not None:
+            grouped_barcodes[neighbor] += explicit_count
+        else:
+            grouped_barcodes[explicit_barcode] = explicit_count
+            # Increment each time a unique barcode is added to grouped_barcodes
+            barcode_groups += 1
+    return grouped_barcodes
+
+
+def find_hamming_neighbor(explicit_barcode, grouped_barcodes, error_hamming_distance):
+    """
+    Takes an explicit barcode, dictionary of grouped barcodes, and allowable Hamming
+    distance, returns a more frequent barcode that is within the Hamming distance of the
+    explicit barcode if one is found.
+    """
+
+    for grouped_barcode in grouped_barcodes.keys():
+        if is_hamming_neighbor(explicit_barcode, grouped_barcode, error_hamming_distance):
+            return grouped_barcode
+    return None
+
+
+def is_hamming_neighbor(string1, string2, error_hamming_distance):
+    """
+    Takes two strings of equal length and allowable Hamming distance, returns whether
+    the two strings are within the allowable Hamming distance.
+    """
+
+    assert len(string1) == len(
+        string2
+    ), "Error: barcode sequences must be the same length"
+    length = len(string1)
+    distance = 0
+    i = 0
+    while i < length and distance <= error_hamming_distance:
+        if string1[i] != string2[i]:
+            distance += 1
+        i += 1
+    if distance <= error_hamming_distance:
+        return True
+    return False
+
+
+def get_true_barcodes(explicit_barcodes_counter, cell_count):
+    """
+    Takes barcode counter and cell count, returns a list of true barcodes sorted by decreasing
+    frequency.
+    """
+
+    true_barcodes = []
+    for explicit_barcode, _ in sorted(
+        explicit_barcodes_counter.items(), key=lambda x: x[1], reverse=True
+    ):
+        true_barcodes.append(explicit_barcode)
+        if len(true_barcodes) == cell_count:
+            break
+    return true_barcodes
+
+
+def write_to_file(true_barcodes, input_file, output_directory):
+    """
+    Takes list of sorted barcodes, input file path, output directory, and saves the list of
+    barcodes in a text document at the output directory location.
+    """
+
+    if output_directory is not None:
+        output_directory.mkdir(parents=True, exist_ok=True)
+    else:
+        output_directory = input_file.parent.resolve()
+
+    output_file = Path(output_directory, "output.txt")
+    with open(output_file, "w") as file:
+        for barcode in true_barcodes:
+            file.writelines(barcode + "\n")
+    print(
+        f"{len(true_barcodes)} true barcodes found. Results located in {output_file.resolve()}"
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=Path, help="filepath to FASTQ file")
+    parser.add_argument("anchor", type=str, help="the known anchor sequence")
+    parser.add_argument(
+        "cell_count",
+        type=int,
+        help="number of cells sequenced/expected true barcodes",
+    )
+    parser.add_argument(
+        "-e",
+        "--error-hamming-distance",
+        type=int,
+        default=1,
+        help="""Hamming distance used for error correction, default is 1, 0 disables error 
+        correction""",
+    )
+    parser.add_argument(
+        "-p",
+        "--proportion-to-consider",
+        type=float,
+        default=1,
+        help="""number of barcode groups to create during error correction proportional to
+        cell-count, default is 1 (error-correction stops once cell_count number of groups are
+        created), this argument is ignored when error_hamming_distance is zero""",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        help="directory to save output list of true barcodes, default is with input file",
+    )
+    args = parser.parse_args()
+    if args.error_hamming_distance < 0:
+        parser.error("error distance must be greater than or equal to 0")
+    if args.proportion_to_consider < 1:
+        parser.error("proportion_to_consider must be greater than or equal to 1")
+    find_barcodes(
+        args.input,
+        args.anchor,
+        args.cell_count,
+        args.error_hamming_distance,
+        args.proportion_to_consider,
+        args.output,
+    )
